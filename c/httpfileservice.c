ec6c29600a915a200ed637874731dfd24f0f27a3
Akayeshmantha <akayeshmantha@apache.org>
2020-07-23 17:23:31 +0200

    fix
    Signed-off-by: Akayeshmantha <akayeshmantha@apache.org>

--- c/httpfileservice.c
+++ c/httpfileservice.c
@@ -206,7 +206,7 @@ void createFileFromUnixDirectoryAndRespond(HttpResponse *response, char *absolut
       char finalFileName[strlen(tarFileName) + strlen(".tar")];
       strcpy(finalFileName, tarFileName);
       strcat(finalFileName,".tar");
-      char *arguments[] = { "tar", "-cf", finalFileName , absolutePath };
+      char *arguments[] = { "'tar'", "'-cf'" , "'aka.tar'" , "'u/ts3420/aish'" };
       execvp("/bin/sh -c", arguments);
       if(doesFileExist(finalFileName)){
         response200WithMessage(response, "Successfully created a file");
